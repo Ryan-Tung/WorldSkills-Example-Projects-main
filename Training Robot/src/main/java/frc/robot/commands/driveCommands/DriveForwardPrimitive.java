1f72635af7d26245aa2e2080785722681ba79ac3
ming <binlai795@gmail.com>
2026-06-25 15:46:06 +0800

    done

---
+++ Training Robot/src/main/java/frc/robot/commands/driveCommands/DriveForwardPrimitive.java
@@ -0,0 +1,50 @@
+package frc.robot.commands.driveCommands;
+
+// Use the 2020/2021 WPILib import paths
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpiutil.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class DriveForwardPrimitive extends CommandBase {
+    private final double targetDistance;
+    private final double targetHeading;
+    
+    private final PIDController pidYAxis;
+    private final PIDController pidZAxis;
+
+    public DriveForwardPrimitive(double distance_mm, double heading_deg) {
+        this.targetDistance = distance_mm;
+        this.targetHeading = heading_deg;
+        
+        addRequirements(RobotContainer.driveTrain);
+
+        pidYAxis = new PIDController(0.005, 0.0, 0.0);
+        pidZAxis = new PIDController(0.02, 0.0, 0.0);
+        
+        pidYAxis.setTolerance(15.0,10.0); 
+    }
+
+    @Override
+    public void initialize() {
+        RobotContainer.driveTrain.resetEncoders();
+    }
+
+    @Override
+    public void execute() {
+        double speedY = MathUtil.clamp(pidYAxis.calculate(RobotContainer.driveTrain.getAverageForwardEncoderDistance(), targetDistance), -0.5, 0.5);
+        double speedZ = MathUtil.clamp(pidZAxis.calculate(RobotContainer.driveTrain.getYaw(), targetHeading), -0.5, 0.5);
+        
+        RobotContainer.driveTrain.holonomicDrive(0.0, speedY, speedZ);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pidYAxis.atSetpoint();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        RobotContainer.driveTrain.holonomicDrive(0.0, 0.0, 0.0);
+    }
+}
--- Training Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Training Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -146,7 +146,7 @@ public class DriveTrain extends SubsystemBase
      */
     public double getLeftEncoderDistance()
     {
-        return leftEncoder.getEncoderDistance();
+        return leftEncoder.getEncoderDistance()*-1;
     }
 
     /**
@@ -176,7 +176,7 @@ public class DriveTrain extends SubsystemBase
      */
     public double getAverageForwardEncoderDistance()
     {
-        return (getLeftEncoderDistance() + getRightEncoderDistance()) / 2; 
+        return (getLeftEncoderDistance() - getRightEncoderDistance()) / 2; 
     }
 
     /**
