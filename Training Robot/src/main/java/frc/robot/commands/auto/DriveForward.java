0a8a354312b3f9802e0cf6c140e563180462c638
ming <binlai795@gmail.com>
2026-06-25 15:29:59 +0800

    yomama

--- Training Robot/src/main/java/frc/robot/commands/auto/DriveForward.java
+++ Training Robot/src/main/java/frc/robot/commands/auto/DriveForward.java
@@ -1,23 +1,12 @@
 package frc.robot.commands.auto;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.driveCommands.SimpleDrive;
-import frc.robot.commands.driveCommands.forward;
-import frc.robot.commands.driveCommands.heading;
-import frc.robot.commands.driveCommands.DriveWithPID;
-
 
 public class DriveForward extends AutoCommand
 {
     public DriveForward ()
     {
-        super(new SequentialCommandGroup( 
-        // x , y , z  //y forward //x 
-
-        new forward(-500,0),
-       
-        new WaitCommand(1)));
-        
+        super(new SimpleDrive(0.0, 0.0 , 0.5).withTimeout(2)); // x , y , z  //y forward //x 
+            
     }
 }
