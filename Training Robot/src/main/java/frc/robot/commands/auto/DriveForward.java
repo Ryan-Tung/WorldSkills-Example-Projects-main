a9dfacacb322502bf9a45268020f7cdeda905570
ming <binlai795@gmail.com>
2026-06-25 11:21:54 +0800

    baseline for extension

--- Training Robot/src/main/java/frc/robot/commands/auto/DriveForward.java
+++ Training Robot/src/main/java/frc/robot/commands/auto/DriveForward.java
@@ -1,12 +1,14 @@
 package frc.robot.commands.auto;
 
 import frc.robot.commands.driveCommands.SimpleDrive;
+import frc.robot.commands.driveCommands.forward;
 
 public class DriveForward extends AutoCommand
 {
     public DriveForward ()
     {
         super(new SimpleDrive(0.0, 0.0 , 0.5).withTimeout(2)); // x , y , z  //y forward //x 
+        new forward(0.0, 0.0 , 0.5).withTimeout(2); // x , y , z  //y forward //x 
             
     }
 }
